1eba4d132d7a846e27e4879af2ae1530716e3306
han sangUk <gtd5309@gmail.com>
2023-03-19 17:14:11 +0900

    vuetemplate

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HelloWorld />
+    <h1>{{ message }}</h1>
+  </div>  
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld.vue'
+
+export default{
+    components:{
+        HelloWorld
+    },
+    data(){
+        return{
+            message:'hello vue'
+        }
+    }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <img 
+      src="~/assets/logo.png"
+      alt="logo" />
+    <div></div>
+  </div>
+</template>
